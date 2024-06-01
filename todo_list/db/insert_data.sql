3efa77003e1d258d3f3b2357d7c5c84d74c40df2
Letalandroid <carlossoncra@gmail.com>
2024-05-31 20:00:16 -0500

    Add insert into on todo_list

---
+++ todo_list/db/insert_data.sql
@@ -0,0 +1,5 @@
+insert into tareas (descripcion) values ('Limpiar');
+insert into tareas (descripcion) values ('Cocinar');
+insert into tareas (descripcion) values ('Darle comida a los perros');
+insert into tareas (descripcion) values ('Comprar cartulina');
+insert into tareas (descripcion) values ('Jugar valito');
